0b0be2f67fea409ead91ea3b6111a354eda2e69f
zeros <luca.perini.illustrations@gmail.com>
2024-02-25 10:07:49 +0100

    Esercizio 2

--- main.js
+++ main.js
@@ -26,6 +26,23 @@ function sfidaDadi() {
 
 }
 
-sfidaDadi()
+//sfidaDadi()
 
 //ESERCIZIO 2
+
+function contatoreFizzbuzz (numero){
+    for(let i = 0; i<= numero; i++){
+        
+        if (i % 15 == 0) {
+            console.log("fizzbuzz")
+        } else if (i % 5 == 0){
+            console.log("buzz")
+        } else if (i % 3 == 0){
+            console.log("fizz")
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+contatoreFizzbuzz(parseInt(prompt("inserisci un numero")))
